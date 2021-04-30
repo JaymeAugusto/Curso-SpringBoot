beebbf7f6f23a8e06fb6d61a4822a75a62e2bc81
Jayme Augusto <ja.guto01@gmail.com>
2021-04-30 18:57:19 -0300

    add cadastro que salva o objeto dentro do banco de dados

--- nutrition/src/main/java/com/example/nutrition/service/CadastroNutricionista.java
+++ nutrition/src/main/java/com/example/nutrition/service/CadastroNutricionista.java
@@ -1,9 +1,17 @@
 package com.example.nutrition.service;
 
+import com.example.nutrition.datasousce.model.Nutricionista;
 import com.example.nutrition.repository.NutricionistaReository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class CadastroNutricionista {
     @Autowired
     private NutricionistaReository nutricionistaReository;
+
+    public void cadastro(Nutricionista nutricionista){
+        nutricionistaReository.saveAndFlush(nutricionista); //salva o objeto dentro do banco de dados
+        //to fazendo essa aula com o meu amor
+    }
 }
