9385a2aec029e72a12612dd6c87bebe1554805f1
Jayme Augusto <ja.guto01@gmail.com>
2021-04-30 19:06:52 -0300

    add nutricionistaController e api buscarNutricionistaPorId

---
+++ nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
@@ -0,0 +1,11 @@
+package com.example.nutrition.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class nutricionistaController {
+
+    public void buscarNutricionistaPorId(){
+
+    }
+}
