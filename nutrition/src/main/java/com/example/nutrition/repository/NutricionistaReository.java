39072b236bc687ab856ad0a998534f0c98e71860
Jayme Augusto <ja.guto01@gmail.com>
2021-04-30 18:46:07 -0300

    add CadastroNutricionista e add Autowired que faz ingeção de dependencia

--- nutrition/src/main/java/com/example/nutrition/repository/NutricionistaReository.java
+++ nutrition/src/main/java/com/example/nutrition/repository/NutricionistaReository.java
@@ -4,4 +4,5 @@ import com.example.nutrition.datasousce.model.Nutricionista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NutricionistaReository extends JpaRepository<Nutricionista, Long> {
+
 }
---
+++ nutrition/src/main/java/com/example/nutrition/service/CadastroNutricionista.java
@@ -0,0 +1,9 @@
+package com.example.nutrition.service;
+
+import com.example.nutrition.repository.NutricionistaReository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CadastroNutricionista {
+    @Autowired
+    private NutricionistaReository nutricionistaReository;
+}
