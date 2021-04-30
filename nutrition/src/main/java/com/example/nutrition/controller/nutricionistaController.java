aea28818918a6fbd2086191be12121ca7eefe192
Jayme Augusto <ja.guto01@gmail.com>
2021-04-30 19:17:09 -0300

    final da aula 2

--- nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
+++ nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
@@ -1,10 +1,12 @@
 package com.example.nutrition.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class nutricionistaController {
 
+    @GetMapping(path = "api/nutricionista-id{}")
     public void buscarNutricionistaPorId(){
 
     }
