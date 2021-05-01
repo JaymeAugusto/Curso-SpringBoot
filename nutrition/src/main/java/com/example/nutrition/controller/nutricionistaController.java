def0644b2bfb7c02109a5d023e6b30e87f88ce6d
Jayme Augusto <ja.guto01@gmail.com>
2021-05-01 20:16:31 -0300

    add get idNutricionista para puxar o banco de dados

--- nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
+++ nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
@@ -1,13 +1,24 @@
 package com.example.nutrition.controller;
 
+import com.example.nutrition.datasousce.model.Nutricionista;
+import com.example.nutrition.repository.NutricionistaReository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class nutricionistaController {
 
-    @GetMapping(path = "api/nutricionista-id{} ")
-    public void buscarNutricionistaPorId(){
+    @Autowired
+    private NutricionistaReository nutricionistaReository;
 
+    @GetMapping(path = "api/nutricionista-id{}")
+    public ResponseEntity<Optional<Nutricionista>> buscarNutricionistaPorId(
+            @PathVariable(name = "id", required = true) Long idNutricionista){
+        return ResponseEntity.ok(nutricionistaReository.findById(idNutricionista));
     }
 }
