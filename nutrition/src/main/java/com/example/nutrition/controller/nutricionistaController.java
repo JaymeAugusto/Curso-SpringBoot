6d07633413ce63392fef9e965979529cc36e02f1
Jayme Augusto <ja.guto01@gmail.com>
2021-05-01 19:12:28 -0300

    inicio aula 3

--- nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
+++ nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class nutricionistaController {
 
-    @GetMapping(path = "api/nutricionista-id{}")
+    @GetMapping(path = "api/nutricionista-id{} ")
     public void buscarNutricionistaPorId(){
 
     }
