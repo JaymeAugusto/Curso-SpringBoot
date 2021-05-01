f0863927c11c8c7cc62111c40c6957bb8648ed80
Jayme Augusto <ja.guto01@gmail.com>
2021-05-01 20:18:08 -0300

    final aula 3

--- nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
+++ nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
@@ -20,5 +20,7 @@ public class nutricionistaController {
     public ResponseEntity<Optional<Nutricionista>> buscarNutricionistaPorId(
             @PathVariable(name = "id", required = true) Long idNutricionista){
         return ResponseEntity.ok(nutricionistaReository.findById(idNutricionista));
+
+        //final
     }
 }
