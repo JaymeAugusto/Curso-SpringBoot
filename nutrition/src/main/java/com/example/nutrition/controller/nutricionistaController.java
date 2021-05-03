f7be1200f7ed84c007f6c9fb246fceab9f20da14
Jayme Augusto <ja.guto01@gmail.com>
2021-05-03 15:01:43 -0300

    add valores manualmente, api funcionando corretamente

--- nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
+++ nutrition/src/main/java/com/example/nutrition/controller/nutricionistaController.java
@@ -6,19 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/apitest")
 public class nutricionistaController {
 
     @Autowired
     private NutricionistaReository nutricionistaReository;
 
-    @GetMapping(path = "api/nutricionista-id{}")
-    public ResponseEntity<Optional<Nutricionista>> buscarNutricionistaPorId(
-            @PathVariable(name = "id", required = true) Long idNutricionista){
-        return ResponseEntity.ok(nutricionistaReository.findById(idNutricionista));
+    @GetMapping(path = "nutricionistas")
+    public List<Nutricionista> buscarNutricionistas(){
+        return nutricionistaReository.findAll();
     }
 }
