a45fc7506df226df1ec8791f2f8993d8127e9fd0
Jayme Augusto <ja.guto01@gmail.com>
2021-05-05 17:41:28 -0300

    implementado try catch no NutricionistaConversor para verificação

--- nutrition/src/main/java/com/example/nutrition/service/NutricionistaConversor.java
+++ nutrition/src/main/java/com/example/nutrition/service/NutricionistaConversor.java
@@ -4,15 +4,38 @@ import com.example.nutrition.datasousce.model.Nutricionista;
 import com.example.nutrition.resouce.model.NutricionistaResource;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class NutricionistaConversor {
 
     public Nutricionista conversor(
             NutricionistaResource nutricionistaResource){
+        try {
+            Nutricionista nutricionista = new Nutricionista();
+
+            Long idPaciente = checkIdPaciente(nutricionistaResource.getIdPaciente());
+
+            LocalDate idade = checkIdade(nutricionistaResource.getIdade());
+
+            nutricionista.setidPaciente(idPaciente);
+            nutricionista.setIdade(idade);
+            nutricionista.setCodigoRegistro(nutricionistaResource.getCondigoRegistro());
+            nutricionista.setNome(nutricionistaResource.getNome());
+
+            return nutricionista;
+
+        }catch (Exception e){
+            throw new NegativeArraySizeException("Falha ao converter o resource para entidade, resource: " + nutricionistaResource);
+        }
 
     }
 
     private Long checkIdPaciente(String idPaciente){
         return Long.parseLong(idPaciente);
+    }
+
+    private LocalDate checkIdade(String idade){
+        return LocalDate.parse(idade);
     }
 }
